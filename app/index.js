53c6743b7fd7960f803261b07c6d666b0432c535
stanislavberger <berger.pro@yandex.ru>
2024-06-07 17:42:04 +0300

    errors debug

---
+++ app/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const router = require('../routrer/index')
+const errorMiddleware = require('../middlewares/error-middleware')
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: err.message });
+});
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+start();
